6ee3769281c30632da6d67ec7ad76fe8a889315a
ashraf-khaled-w <ashraf.khaled.w@gmail.com>
2025-05-16 23:01:14 +0300

    m

--- src/components/SecAbout/SecAbout.jsx
+++ src/components/SecAbout/SecAbout.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-import styles from './SecAbout.module.css'
-
+import React from "react";
+import styles from "./SecAbout.module.css";
 
 function SecAbout() {
-    return <>
-    
+  return (
+    <>
+      <h2>نبذة عن الدكتور</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo repudiandae blanditiis
+        consectetur fuga ducimus assumenda facilis molestias eos. Laboriosam iure delectus,
+        excepturi amet laborum sunt exercitationem adipisci quidem consequuntur? Molestias
+        labore sit quidem vero laborum veritatis, excepturi, ab voluptas iste quasi nemo ad
+        commodi quae vel, repellat illo quod ducimus laudantium expedita at dignissimos
+        consectetur minus? Illum nulla corrupti perspiciatis officiis itaque labore, minus
+        dolorem maiores doloremque at quasi pariatur officia reiciendis cum alias dolor? Illo
+        dolorem sint delectus dolor autem id, molestiae iusto ratione odit aliquam nesciunt
+        ullam voluptates vero itaque modi soluta est? Ipsum maiores excepturi laboriosam
+        repudiandae, eaque sequi incidunt, in sapiente illum dolor amet iste atque corrupti
+        rerum quos sed voluptatibus. Distinctio ullam eos assumenda animi nemo iste, quaerat
+        minus rerum nam debitis reprehenderit nisi aperiam asperiores autem veritatis. Nihil
+        officiis ipsam totam, atque accusamus at voluptas ratione consequuntur debitis magnam
+        dolorem, reprehenderit, dicta sunt laborum assumenda minima velit odio doloremque
+        inventore doloribus saepe quae voluptatem mollitia. Aliquam cum exercitationem iste,
+        numquam nihil architecto recusandae dolores dicta est. Nesciunt laborum veritatis
+        placeat. Reiciendis quas nulla voluptatibus molestiae culpa fuga error est, aut placeat
+        minima tempore ea dolor hic suscipit quae architecto voluptate alias. Expedita,
+        dignissimos beatae.
+      </p>
     </>
+  );
 }
 
-export default SecAbout
+export default SecAbout;
