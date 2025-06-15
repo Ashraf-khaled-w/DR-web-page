cd6f1be353973f7ba2f8c9286eeea01db1f884b2
hadeer <hadeer.abelgawad44@gmail.com>
2025-06-15 04:31:42 +0300

    'hadeer'

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ import NavElement from '../UI/NavElement/NavElement'
 
 function Navbar() {
     return <>
-    <nav className='flex justify-between align-baseline items-center w-[vw] py-5 shadow bg-gray-200 border-b-2 border-[#FA812F] rounded-b-2xl overflow-hidden px-4'>
+    <nav className='flex flex-col lg:flex-row md:flex-row justify-between align-baseline items-center gap-5 w-[vw] py-5 shadow bg-gray-200 border-b-2 border-[#FA812F] rounded-b-2xl overflow-hidden px-4'>
         <div className="logo">
             Dr/ Mohamed El-Sayed Abdel-Aal
         </div>
-        <ul className='flex space-x-4' dir='rtl'>
+        <ul className='flex flex-col lg:flex-row md:flex-row items-center space-x-4 space-y-4 lg:space-y-0 lg:space-x-0 md:space-x-0' dir='rtl'>
             <NavElement path='/'>الصفحة الرئيسية</NavElement>
             <NavElement path='/about'> عن الدكتور</NavElement>
             <NavElement path='/service'>الخدمات</NavElement>
--- src/components/UI/NavElement/NavElement.jsx
+++ src/components/UI/NavElement/NavElement.jsx
@@ -8,7 +8,7 @@ function NavElement({ path, children, className = '' }) {
       <NavLink
         to={path}
         className={({ isActive }) =>
-          `px-4 py-2 rounded-full transition-all duration-200 font-bold
+          `px-4 py-2 rounded-full transition-all duration-200 font-bold overflow-hidden whitespace-nowrap text-sm 
           ${isActive ? 'bg-[#FA812F] text-white shadow hover:text-gray-700' : 'text-[#309898] hover:bg-[#309898]/10 hover:text-gray-500'} ${className}`
         }
       >
